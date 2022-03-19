aa4a440f449d9cb342d0b834684e53f87202f3fb
Golam Hossain <rabby28hossain@gmail.com>
2022-03-19 16:28:04 -0400

    fixing deployment issues

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
-import 'normalize.css';
 
 /**
  * Defines the root application component.
